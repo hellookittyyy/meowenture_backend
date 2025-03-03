a9fb849e1a220fa71cee8a1519362db1a3c87229
ann-bychyshyn <annabychyshynanna@gmail.com>
2025-03-03 23:00:17 +0200

    changed urls for profile and login

--- backend/database/urls.py
+++ backend/database/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    # path('login/', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     # path('logout/', views.logout_view, name='logout'),
-    # path('profile/', views.profile_view, name='profile'),
+    path('profile/', views.profile_view, name='profile'),
     # path('change-password/', views.change_password_view, name='change_password'),
 ]
