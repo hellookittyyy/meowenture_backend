184df9ed12b0fdb90cd692dbe40c853cf007f553
Hello Kitty <katya.shapovalova3012@gmail.com>
2025-03-03 20:17:45 +0200

    created models in database app

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,5 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+from django.utils import timezone
 
-class User(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=255)
+class CustomUser(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+    username = models.CharField(_("username"), max_length=150, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True, default='profile_images/default.png')
+
+    USERNAME_FIELD = "email" 
+    REQUIRED_FIELDS = ["username"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+    
+    @property
+    def profile_image_url(self):
+        if self.profile_image:
+            return self.profile_image.url
+        return '/static/profile_images/default.png'
+    
+# class OTP(models.Model):
+#     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+#     otp = models.CharField(max_length=6)
+#     created_at = models.DateTimeField()
+
+#     def __str__(self):
+#         return f"OTP for {self.user.email}"
