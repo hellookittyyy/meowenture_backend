f49806769be18791e1d9b26eec3619156dbb3037
Hello Kitty <katya.shapovalova3012@gmail.com>
2025-05-02 00:35:10 +0300

    add models

--- backend/database/models.py
+++ backend/database/models.py
@@ -11,7 +11,7 @@ class CustomUser(AbstractUser):
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
-    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True, default='profile_images/default.png')
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, default='profile_images/default.png')
 
     USERNAME_FIELD = "email" 
     REQUIRED_FIELDS = ["username"]
@@ -26,6 +26,40 @@ class CustomUser(AbstractUser):
         if self.profile_image:
             return self.profile_image.url
         return '/static/profile_images/default.png'
+
+class Choice(models.Model):
+    dialog = models.ForeignKey('Dialog', on_delete=models.CASCADE, related_name='choices')
+    text = models.CharField(max_length=255)
+    next_dialog = models.ForeignKey('Dialog', on_delete=models.CASCADE, related_name='next_dialogues')
+    final_points = models.IntegerField(default=0)
+    level_points = models.IntegerField(default=0)
+
+class Block(models.Model):
+    type = models.CharField(max_length=255, choices=[('platform', 'Platform'), ('obstacle', 'Obstacle'), ('background obj', 'Background obj'), ('item', 'Item'), ('enemy', 'Enemy')], default='platform')
+    coordinates = models.CharField(max_length=255, default='0,0')
+    size = models.CharField(max_length=255, default='1,1')
+    image = models.ImageField(upload_to='block_images/', default ='block_images/default.png')
+    is_start = models.BooleanField(default=False)
+    is_finish = models.BooleanField(default=False)
+
+class Level(models.Model):
+    background = models.ImageField(upload_to='level_backgrounds/', default='level_backgrounds/default.png')
+    level_number = models.IntegerField()
+    block = models.ManyToManyField(Block)
+    next_dialog = models.ForeignKey('Dialog', on_delete=models.CASCADE, related_name='next_dialogs', default='1')
+
+class Character(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='character_images/', default ='character_images/default.png')
+
+class Dialog(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name='dialogs')
+    next_dialog = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, related_name='next_dialoges')
+    next_level = models.ForeignKey('Level', on_delete=models.CASCADE, related_name='dialogs', null=True)
+    text = models.TextField()
+    is_choice = models.BooleanField(default=False)
+    stage_number = models.IntegerField(default=1)
+    background = models.ImageField(upload_to='dialog_backgrounds/', default='dialog_backgrounds/default.png')
     
 # class OTP(models.Model):
 #     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
